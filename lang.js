7b18c093bd6fa1c2d2dc5be7327ef2c3d4f6fa5c
Tetiana Chykalova <chikalova.t@gmail.com>
2022-09-20 17:29:29 +0200

    done

---
+++ lang.js
@@ -0,0 +1,16 @@
+const langArr = {
+    "title": {
+        "ua": "Привіт, світ",
+        "en": "hello, world",
+    },
+    "text": {
+        "ua": "шукаю віддалену вакансію Front-end розробника або HTML-верстальника рівня trainee. Маю досвід роботи в офісі, тож вмію працювати в команді та дотримуватись дедлайнів. Комерційного досвіду не маю. Але маю велике бажання вчитись.",
+        "en": "I am looking for a remote vacancy for a Front-end developer or trainee-level HTML developer. I have experience working in an office, so I know how to work in a team and meet deadlines. I have no commercial experience. But I have a great desire to learn.",
+    },
+}
+const langTitle = {
+    "unit": {
+        "ua": "багатомовний сайт",
+        "en": "multilingual page"
+    },
+}
